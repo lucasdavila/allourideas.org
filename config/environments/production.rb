e05637def78702821c96cf9360ad9df7b27680b5
Lucas D'Avila <lucassdvl@gmail.com>
2012-05-09 13:15:25 -0300

    Configs for production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,7 +8,7 @@ config.cache_classes = true
 # config.logger = SyslogLogger.new
 
 # Full error reports are disabled and caching is turned on
-config.action_controller.consider_all_requests_local = false
+config.action_controller.consider_all_requests_local = true
 config.action_controller.perform_caching             = true
 
 # Enable serving of images, stylesheets, and javascripts from an asset server
@@ -26,15 +26,18 @@ if File.exists?(extra_conf)
   require extra_conf
 end
 
+# allourideas app
+HOST                = 'localhost:3001'
 
-#pains me to do this, but can't seem to get environment variables set in EY
-#will change login after demo
-HOST = ENV['ALLOURIDEAS_HOST']
-API_HOST = ENV['API_HOST']
-PAIRWISE_USERNAME = ENV['PAIRWISE_USERNAME']
-PAIRWISE_PASSWORD = ENV['PAIRWISE_PASSWORD']
-IP_ADDR_HASH_SALT = ENV['IP_ADDR_HASH_SALT'] #prevent dictionary attacks on stored ip address hashes
+# pairwise api
+API_HOST            = 'http://localhost:3000'
+PAIRWISE_USERNAME   = 'SET_HERE_PAIRWISE_API_USERNAME'
+PAIRWISE_PASSWORD   = 'PAIRWISE_API_PASSWORD'
 
-PHOTOCRACY_USERNAME = ENV['PHOTOCRACY_USERNAME']
-PHOTOCRACY_PASSWORD = ENV['PHOTOCRACY_PASSWORD']
-PHOTOCRACY_HOST = 'photocracy.org'
+# photocracy api
+PHOTOCRACY_HOST     = 'photocracy.org'
+PHOTOCRACY_USERNAME = 'PHOTOCRACY_API_USERNAME'
+PHOTOCRACY_PASSWORD = 'PHOTOCRACY_API_PASSWORD'
+
+# prevent dictionary attacks on stored ip address hashes
+IP_ADDR_HASH_SALT   = '2039d9ds9ufsdioh2394230'
